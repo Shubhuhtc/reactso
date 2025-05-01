21c73e4efe926a700574b4472264048742cc68f2
Shubhuhtc <152644489+Shubhuhtc@users.noreply.github.com>
2025-05-01 12:15:51 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from React!</h1>
+      <p>This project is configured to use a Nexus npm registry.</p>
+    </div>
+  );
+}
+
+export default App;
